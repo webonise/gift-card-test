551720768c1871f522fa23fe07c09ed4abc6210d
Richa Sharma <richa@weboniselab.com>
2013-06-05 18:38:30 +0530

    put var in gitignore

--- lib/Zend/Cache/Backend/File.php
+++ lib/Zend/Cache/Backend/File.php
@@ -88,7 +88,7 @@ class Zend_Cache_Backend_File extends Zend_Cache_Backend implements Zend_Cache_B
      * @var array available options
      */
     protected $_options = array(
-        'cache_dir' => null,
+        'cache_dir' => 'tmp/',
         'file_locking' => true,
         'read_control' => true,
         'read_control_type' => 'crc32',
